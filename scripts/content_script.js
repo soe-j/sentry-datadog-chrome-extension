1ac3738edc47b965dc971f80fe3da54e36d22361
soe-j <7115203+soe-j@users.noreply.github.com>
2024-06-18 14:24:02 +0900

    feat: update query to get element cause sentry updated page

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -4,10 +4,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     alert('Not found [aria-label="environment"] Element');
     return;
   }
-  const env = envElem.innerText.split("\n")[1];
+  const envRaw = envElem.innerText.split("\n")[1];
+  const env = (() => {
+    if (envRaw.includes("develop")) return "develop";
+    return envRaw;
+  })();
 
-  const userIdElem = document.querySelector('[aria-label="user"]');
-  const userId = userIdElem ? userIdElem.innerText.split("\n")[1] : null;
+  const userIdElem = document
+    .querySelector('[data-sentry-element="CardPanel"]')
+    .querySelector('[data-sentry-element="ValueSection"]');
+
+  if (!userIdElem) {
+    if (!window.confirm("userIdElem not found. continue?")) {
+      return;
+    }
+  }
+
+  const userId = userIdElem ? userIdElem.innerText : null;
 
   const dateElem = document.querySelector("time");
   if (!dateElem) {
